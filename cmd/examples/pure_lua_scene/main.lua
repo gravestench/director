91281def7aa987fe97e14b6fa6bf99a20f161dcb
gravestench <dknuth0101@gmail.com>
2021-09-06 14:08:10 -0700

    updated lua scene example

--- cmd/examples/pure_lua_scene/main.lua
+++ cmd/examples/pure_lua_scene/main.lua
@@ -1,17 +1,21 @@
 math.randomseed(os.time())
 
 shapes = {}
+elapsed = 0
 
 function init()
-    for i = 100,0,-1
+    for i = 0,100,1
     do
-        shapes[i] = randomShape()
+        shapes[i] = randShape()
     end
 end
 
-function update()
+function update(timeDelta)
+    elapsed = elapsed + timeDelta
     for _, e in ipairs(shapes) do
         updatePosition(e:id())
+        updateRotation(e:id())
+        updateOrigin(e:id())
     end
 end
 
@@ -35,24 +39,57 @@ function updatePosition(eid)
     trs.translation(tx, ty, tz)
 end
 
+function updateRotation(eid)
+    trs, found = components.transform.get(eid)
+    if not found then
+        return
+    end
+
+
+    rx, ry, rz = trs.rotation()
+    ry = ry + 1
+
+    trs.rotation(rx, ry, rz)
+end
+
+function updateOrigin(eid)
+    origin, found = components.origin.get(eid)
+    if not found then
+        return
+    end
+
+    n = elapsed / 1000000000
+
+    ox, oy, oz = origin.xyz()
+    ox = math.cos(n)
+    oy = math.sin(n)
+
+    origin.xyz(ox, oy, oz)
+end
+
 function coinFlip()
     v = math.random() * 2
     return v > 1
 end
 
-function rcc()
+function randColorComponent()
     v = math.random()
     return string.format("%02x", v * 255)
 end
 
-function randomShape()
+function randColor()
+    r, g, b = randColorComponent(), randColorComponent(), randColorComponent()
+    return "#" .. r .. g .. b
+end
+
+function randShape()
     x = math.random(0, 1024)
     y = math.random(0, 768)
     w = math.random(0, 150/2) * 2
     h = math.random(0, 150/2) * 2
 
-    fill = "#" .. rcc() .. rcc() .. rcc()
-    stroke = "#" .. rcc() .. rcc() .. rcc()
+    fill = randColor()
+    stroke = randColor()
 
     if coinFlip() then
         e = rectangle.new(x, y, w, h, fill, stroke)
