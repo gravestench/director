4a192e5a55635f7c4d124cda9f60e500aa33b73b
gravestench <dknuth0101@gmail.com>
2021-09-12 10:34:39 -0700

    fixing ocmment for common.luaTypeExport

--- pkg/common/lua_type_export.go
+++ pkg/common/lua_type_export.go
@@ -4,8 +4,8 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
-// LuaTypeExport is a collection of all the information we need about a Go type in order to export it for
-// use in Lua scripts.
+// LuaTypeExport is a collection of all the information we
+// need about a Go type in order to export it for use in Lua scripts.
 type LuaTypeExport struct {
 	Name            string
 	ConstructorFunc lua.LGFunction
